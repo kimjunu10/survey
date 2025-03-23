5fc8e3f89636c94edede42905d77777ae975b9fe
kimjunu10 <kjw4523705@naver.com>
2025-03-23 18:53:47 +0900

    real_final_commit

--- back/server.js
+++ back/server.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
 
